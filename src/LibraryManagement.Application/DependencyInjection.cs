373131b739fadf44eb80405e1e3a5d33574dd02a
aeronhappy <maligaya.ads@gmail.com>
2025-08-06 14:41:50 +0800

    dependency injection naiwan

---
+++ src/LibraryManagement.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagement.API.Services.Interface;
+using LibraryManagement.Application.CommandHandler;
+using LibraryManagement.Application.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace LibraryManagement.Application
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // Register Services
+            services.AddScoped<IBookCommandSerice, BookCommandService>();
+            services.AddScoped<IMemberCommandService, MemberCommandService>();
+            services.AddScoped<IAuthenticationCommandService, AuthenticationCommandService>();
+            services.AddScoped<IRoleCommandService, RoleCommandService>();
+            services.AddScoped<IBorrowingCommandService, BorrowingCommandService>();
+
+
+            return services;
+        }
+    }
+}
